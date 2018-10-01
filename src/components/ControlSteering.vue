c383223016e96016556ccbaa0ba33bd3a4e4bbb5
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2018-10-02 00:44:46 +0300

    Add label tags & improve radio button automation on clicks

--- src/components/ControlSteering.vue
+++ src/components/ControlSteering.vue
@@ -2,18 +2,18 @@
   <div id="steering">
     <div class="steering-input">
       <input type="radio" id="type" value="cc" v-model="type">
-      COG
-      <input v-model.trim="cc">
+      <label for="cc" @click="type = 'cc'">COG</label>
+      <input id="cc" @click="type = 'cc'" v-model.trim="cc">
     </div>
     <div class="steering-input">
       <input type="radio" id="type" value="twa" v-model="type">
-      TWA
-      <input v-model.trim="twa">
+      <label for="twa" @click="type = 'twa'">TWA</label>
+      <input id="twa" @click="type = 'twa'" v-model.trim="twa">
     </div>
     <div class="steering-input">
       <input type="checkbox" id="delayOn" v-model="delayOn">
-      Delay for
-      <input v-model.trim="delay">
+      <label for="delay" @click="delayOn = true">Delay for</label>
+      <input id="delay" v-model.trim="delay">
     </div>
     <control-steering-polar/>
   </div>
