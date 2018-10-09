b21756ebfa9332422e941ff4fa4cb405db6be816
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2018-10-09 11:21:32 +0300

    Fix inconsistent month parsing, it's only field starting from 0

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,13 +2,13 @@ export function UTCtoMsec(dateString) {
   const regex = new RegExp(/^(\d{4})\/([01]\d)\/([0-3]\d) ([012]\d):([0-5]\d):([0-5]\d)/);
 
   const s = regex.exec(dateString);
-  const ms = Date.UTC(s[1], s[2], s[3], s[4], s[5], s[6]);
+  const ms = Date.UTC(s[1], parseInt(s[2])-1, s[3], s[4], s[5], s[6]);
 
   if (isNaN(ms)) {
     return null;
   }
   const d = new Date(ms);
-  const check = [d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(),
+  const check = [d.getUTCFullYear(), d.getUTCMonth()+1, d.getUTCDate(),
                  d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds()];
   for (let i = 0; i < check.length; i++) {
     if (check[i] !== parseInt(s[i+1])) {
