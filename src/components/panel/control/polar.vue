c158595b10e31036514509d816bb590afc68e076
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2018-11-25 00:30:45 +0200

    Basic polar drawing done

--- src/components/panel/control/polar.vue
+++ src/components/panel/control/polar.vue
@@ -1,17 +1,158 @@
 <template>
-  <div>
-    Polar
-
-    not implemented yet!
+  <div v-if="this.$store.state.boat.polar.loaded">
+    <div id="bg">
+      <canvas id="labels" ref="labels"/>
+      <div id="polar">
+        <canvas id="polarbg" ref="polarbg"/>
+        <canvas id="polarfg" ref="polarfg"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import {MS_TO_KNT, windToColor} from '../../../lib/sol.js';
+
 export default {
   name: 'ControlSteeringPolar',
   data () {
     return {
-
+      curves: [3, 6, 9, 12, 15, 20, 25, 30],
+      interval: 1,
+      margins: 2 * 20,
     }
+  },
+  computed: {
+    bgCurves () {
+      let res = [];
+      for (let knots of this.curves) {
+        res.push(this.$store.getters['boat/polar/curve'](knots, this.interval));
+      }
+      return res;
+    },
+    fgCurve () {
+      const knots = this.$store.state.boat.instruments.tws.value * MS_TO_KNT;
+      return this.$store.getters['boat/polar/curve'](knots, this.interval);
+    },
+    maxSpeed () {
+      /* 105% of the real max BS to give a small breathing room for the curves */
+      return Math.max(...this.bgCurves.map(c => c.maxspeed.speed), 0) * 1.05;
+    },
+    maxWidth () {
+      return this.$refs.labels.width - this.margins;
+    },
+    gridIntervalKnots () {
+      return Math.ceil(this.maxSpeed / (this.maxWidth / 20));
+    },
+    gridIntervalPixels () {
+      return Math.floor(this.maxWidth / Math.ceil(this.maxSpeed / this.gridIntervalKnots));
+    },
+    gridMaxKnots () {
+      return Math.floor(this.maxWidth / this.gridIntervalPixels)
+    },
+    gridScale () {
+      return this.gridIntervalPixels / this.gridIntervalKnots;
+    },
+    gridOrigoY () {
+      const maxVmgUp = Math.max(...this.bgCurves.map(c => c.maxvmg.up.vmg), 0);
+      return Math.ceil(maxVmgUp * this.gridScale * 1.05);
+    },
+    gridSize () {
+      return {
+        x: this.gridMaxKnots * this.gridIntervalPixels,
+        y: this.gridOrigoY + this.gridMaxKnots * this.gridIntervalPixels,
+      };
+    },
+
+    /* Deps access to trigger redraw correctly */
+    bgNeedRedraw () {
+      this.bgCurves;
+      this.gridSize;
+
+      return Date.now();
+    },
+    fgNeedRedraw () {
+      this.fgCurve;
+      this.gridSize;
+      this.$store.state.boat.instruments.twa.value;
+      this.$store.state.boat.instruments.perf.value;
+
+      return Date.now();
+    },
+  },
+  methods: {
+    drawBg () {
+      console.log("drawbg");
+      console.log(this.gridSize);
+      console.log(this.gridIntervalKnots);
+      console.log(this.gridIntervalPixels);
+      console.log(this.gridMaxKnots);
+      console.log(this.gridOrigoY);
+
+      this.$refs.polarbg.width = this.gridSize.x;
+      this.$refs.polarbg.height = this.gridSize.y;
+
+      let ctx = this.$refs.polarbg.getContext('2d');
+      ctx.save();
+      ctx.strokeStyle = '#aaa';
+
+      ctx.beginPath();
+      ctx.moveTo(0, this.gridSize.y);
+      ctx.lineTo(0, 0);
+      const xBorderEnd = Math.sqrt(Math.pow(this.gridMaxKnots * this.gridScale, 2) -
+                                   Math.pow(this.gridOrigoY, 2));
+      ctx.lineTo(xBorderEnd, 0);
+      ctx.stroke();
+
+      ctx.translate(0, this.gridOrigoY);
+      let i = 1;
+      while (i * this.gridIntervalKnots <= this.gridMaxKnots) {
+        ctx.beginPath();
+        ctx.arc(0, 0, i * this.gridScale, 1.5 * Math.PI, 0.5 * Math.PI);
+        ctx.stroke();
+        i++;
+      }
+
+      for (let curve of this.bgCurves) {
+        ctx.strokeStyle = windToColor(curve.knots);
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        for (let pt of curve.values) {
+          ctx.lineTo(Math.sin(pt.twa) * pt.speed * this.gridScale,
+                     -Math.cos(pt.twa) * pt.speed * this.gridScale);
+        }
+        ctx.stroke();
+      }
+      ctx.restore();
+
+      this.$refs.polarfg.width = this.gridSize.x;
+      this.$refs.polarfg.height = this.gridSize.y;
+
+      this.drawFg();
+    },
+    drawFg () {
+
+    },
+  },
+  mounted () {
+    this.drawBg();
   }
 }
+</script>
+
+<style scoped>
+#bg {
+  position: absolute;
+  width: 100%;
+}
+#polar {
+  position: relative;
+  top: 20px;
+  left: 20px;
+}
+#polarbg, #polarfg, #labels {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+</style>
--- src/store/modules/polar.js
+++ src/store/modules/polar.js
@@ -1,9 +1,12 @@
-import { radToDeg, bsearchLeft, interpolateFactor, linearInterpolate } from '../../lib/utils.js';
+import { degToRad, bsearchLeft, interpolateFactor, linearInterpolate } from '../../lib/utils.js';
+import { speedTowardsBearing } from '../../lib/nav.js';
+import { MS_TO_KNT } from '../../lib/sol.js';
 
 export default {
   namespaced: true,
 
   state: {
+    loaded: false,
     twsval: [],
     twaval: [],
     bs: [],
@@ -12,7 +15,7 @@ export default {
   mutations: {
     set(state, polar) {
       state.twsval = polar.tws_splined.split(/\s+/).map(parseFloat);
-      state.twaval = polar.twa_splined.split(/\s+/).map(parseFloat);
+      state.twaval = polar.twa_splined.split(/\s+/).map(parseFloat).map(degToRad);
 
       let rows = polar.bs_splined.split(/;\s*/);
 
@@ -33,19 +36,20 @@ export default {
       Object.freeze(state.twsval);
       Object.freeze(state.twaval);
       Object.freeze(state.bs);
+      state.loaded = true;
     },
   },
 
   getters: {
-    twsmsTwaToSpeed: (state) => (twsms, twa) => {
-      twa = radToDeg(Math.abs(twa));
+    getSpeed: (state) => (twsms, twa) => {
+      twa = Math.abs(twa);
 
       let twsidx = bsearchLeft(state.twsval, twsms);
       let twaidx = bsearchLeft(state.twaval, twa);
-      if (twsidx === state.twsval.length - 1) {
+      if (twsidx > 0) {
         twsidx--;
       }
-      if (twaidx === state.twaval.length - 1) {
+      if (twaidx > 0) {
         twaidx--;
       }
 
@@ -64,6 +68,44 @@ export default {
                                   state.bs[twaidx+1][twsidx+1]);
 
       return linearInterpolate(twaFactor, a, b);
+    },
+
+    curve: (state, getters) => (knots, interval) => {
+      const ms = knots / MS_TO_KNT;
+      let curve = {
+        ms: ms,
+        knots: knots,
+        maxvmg: {
+          up: { twa: 0, vmg: 0, },
+          down: { twa: 0, vmg: 0, },
+        },
+        maxspeed: {
+          twa: 0, speed: 0,
+        },
+        values: [],
+      }
+      for (let twad = 0; twad <= 180; twad += interval) {
+        const twa = degToRad(twad);
+
+        const speed = getters['getSpeed'](ms, twa);
+        curve.values.push({twa: twa, speed: speed});
+
+        if (speedTowardsBearing(speed, twa, 0) > curve.maxvmg.up.vmg) {
+          curve.maxvmg.up.vmg = speedTowardsBearing(speed, twa, 0);
+          curve.maxvmg.up.twa = twa;
+        }
+        if (speedTowardsBearing(speed, twa, Math.PI) < curve.maxvmg.down.vmg) {
+          curve.maxvmg.down.vmg = speedTowardsBearing(speed, twa, Math.PI);
+          curve.maxvmg.down.twa = twa;
+        }
+        if (speed > curve.maxspeed.speed) {
+          curve.maxspeed.speed = speed;
+          curve.maxspeed.twa = twa;
+        }
+      }
+      // ADDME: refine maxvmg calculations!
+
+      return curve;
     },
   },
 }
