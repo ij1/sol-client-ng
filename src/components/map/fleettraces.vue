6124b810f2ef2672436980b0b19912cab6f06659
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2018-12-05 22:41:29 +0200

    Move store use into two helpers to make conditions shorters

--- src/components/map/fleettraces.vue
+++ src/components/map/fleettraces.vue
@@ -2,7 +2,7 @@
   <l-layer-group>
     <boat-trace
       v-if = "this.showLeaderTrace"
-      :id = "this.$store.state.race.fleet.leader"
+      :id = "this.leaderId"
     />
     <boat-trace
       v-for = "trace in this.otherTraces"
@@ -24,15 +24,21 @@ export default {
   },
 
   computed: {
+    playerId () {
+      return this.$store.state.boat.id;
+    },
+    leaderId () {
+      return this.$store.state.race.fleet.leader;
+    },
     showLeaderTrace () {
-      return (this.$store.state.race.fleet.leader !== null) &&
-             (this.$store.state.race.fleet.leader !== this.$store.state.boat.id);
+      return (this.leaderId !== null) &&
+             (this.leaderId !== this.playerId);
     },
     otherTraces () {
       return this.$store.state.race.fleet.selected.concat(
         this.$store.state.race.fleet.hover
       ).sort().filter((item, pos, arr) => {
-        return !pos || (item !== arr[pos - 1] && item !== this.$store.state.race.fleet.leader)
+        return !pos || (item !== arr[pos - 1] && item !== this.leaderId)
       });
     },
   },
