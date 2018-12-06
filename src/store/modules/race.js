35f9894c4192ec2fca5c5a928a47d4bea90c159e
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2018-12-06 15:36:53 +0200

    Move parsing of the race course out of committer

--- src/store/modules/race.js
+++ src/store/modules/race.js
@@ -1,4 +1,3 @@
-import Vue from 'vue';
 import L from 'leaflet';
 import raceMessageModule from './racemessages.js';
 import fleetModule from './fleet.js';
@@ -23,8 +22,25 @@ export default {
 
   mutations: {
     init (state, raceInfo) {
-      state.boundary = [L.latLng(raceInfo.minlat, raceInfo.minlon),
-                        L.latLng(raceInfo.maxlat, raceInfo.maxlon)];
+      state.boundary = raceInfo.course.boundary;
+      state.route = raceInfo.course.route;
+      state.finish = raceInfo.course.finish;
+      delete raceInfo.course;
+      state.info = raceInfo;
+      state.loaded = true;
+    },
+  },
+
+  getters: {
+    parseCourse: () => (raceInfo) => {
+      let course = {
+        boundary: [
+          L.latLng(raceInfo.minlat, raceInfo.minlon),
+          L.latLng(raceInfo.maxlat, raceInfo.maxlon)
+        ],
+        route: [],
+        finish: [],
+      };
 
       for (let i = 0; i < raceInfo.course.waypoint.length; i++) {
         let waypoint = raceInfo.course.waypoint[i];
@@ -40,29 +56,27 @@ export default {
 
         /* Calculate bearing from prev WP to this WP ... */
         if (i > 0) {
-          const prevwp = PROJECTION.project(state.route[i - 1].latLng);
+          const prevwp = PROJECTION.project(course.route[i - 1].latLng);
           const thiswp = PROJECTION.project(waypoint.latLng);
           const bearing = atan2Bearing(thiswp.x - prevwp.x, thiswp.y - prevwp.y);
-          state.route[i - 1].nextWpBearing = bearing;
+          course.route[i - 1].nextWpBearing = bearing;
 
           /* ...and which side to pass the prev WP */
           if (i > 1) {
-            const turn = minTurnAngle(state.route[i - 2].nextWpBearing,
+            const turn = minTurnAngle(course.route[i - 2].nextWpBearing,
                                       bearing);
-            state.route[i - 1].side = (turn < 0 ? "Port" : "Starboard");
+            course.route[i - 1].side = (turn < 0 ? "Port" : "Starboard");
           }
         }
-        Vue.set(state.route, idx, waypoint);
+        course.route[idx] = waypoint;
       }
       // ADDME: calculate finish line end points
-      delete raceInfo.course.waypoint;
-      state.info = raceInfo;
-      state.loaded = true;
+      return course;
     },
   },
 
   actions: {
-    fetchAuthRaceinfo ({rootState, rootGetters, commit, dispatch}) {
+    fetchAuthRaceinfo ({rootState, getters, rootGetters, commit, dispatch}) {
       /* Initialize time before boat/wx is fetched to avoid issues */
       const now = rootGetters['time/now']();
       commit('boat/instruments/initTime', now, {root: true});
@@ -85,6 +99,7 @@ export default {
           delete raceInfo.boat;
           delete raceInfo.chatrooms;
           raceInfo.start_time = UTCToMsec(raceInfo.start_time);
+          raceInfo.course = getters['parseCourse'](raceInfo);
           commit('init', raceInfo);
 
           commit('boat/polar/set', polarRawData, {root: true});
