a4a27aeb30839b8a5f69e1a0691472f2d33506c5
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2019-01-07 22:57:32 +0200

    Try 127.0.0.1 as host to avoid non-localhost binds

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   devServer: {
-    host: 'localhost',
+    host: '127.0.0.1',
     proxy: {
       // proxy all requests starting with /proxy to jsonplaceholder
       '/proxy': {
