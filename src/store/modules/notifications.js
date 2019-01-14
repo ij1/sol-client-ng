0b835e506890ab046a08f3872496c4cf30180e07
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2019-01-14 23:38:33 +0200

    Circumvent mutation limitation by wrapping it into action
    The action can access getters so it can set the metadata
    properly. Sadly this is quite annoying limitation in Vue
    requiring stupid duplication & scattering code around
    (but at least I now figured out how to work-around the
    limitation, it will be useful elsewhere too I think to
    avoid moving stuff into caller which is even worse than
    putting it into action).

--- src/store/modules/notifications.js
+++ src/store/modules/notifications.js
@@ -7,17 +7,23 @@ export default {
   },
 
   mutations: {
+    /* Call add action instead to get notification metadata set correctly */
     add (state, notification) {
-      notification.time = Date.now();
-      if (typeof notification.color === 'undefined') {
-        notification.color = 'black';
-      }
       notification.id = state.idCount++;
       state.notifications.push(notification);
-      
     },
     clear (state) {
       state.notifications = [];
+    },
+  },
+
+  actions: {
+    add ({rootGetters, commit}, notification) {
+      notification.time = rootGetters['time/now']();
+      if (typeof notification.color === 'undefined') {
+        notification.color = 'black';
+      }
+      commit('add', notification);
     },
   },
 }
