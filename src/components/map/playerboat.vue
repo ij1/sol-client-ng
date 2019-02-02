b2811d27ff16aa2b8b8ba27643a6df93345d5387
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2019-02-02 21:33:50 +0200

    More tweaks to player boat icon drawing

--- src/components/map/playerboat.vue
+++ src/components/map/playerboat.vue
@@ -30,14 +30,23 @@ export default {
     path () {
       return 'M 8,22 C 5 10, 9 12, 11 0 C 13 12, 17 10,14 22 Z';
     },
+    iconCenter () {
+      return 11;
+    },
+    iconSize () {
+      return this.iconCenter * 2;
+    },
     myBoatIcon() {
-      const svg = "<svg xmlns='http://www.w3.org/2000/svg' width='22px' height='22px'><g transform='rotate(" +
-        this.boatCourse +
-        " 11 11)'><path d='" + this.path + "' fill='none' stroke-opacity='1' stroke-width='2' stroke='#ff00ff'/></g></svg>";
+      const svg = "<svg xmlns='http://www.w3.org/2000/svg' width='" +
+                  this.iconSize + "px' height='" +
+                  this.iconSize + "px'>" +
+        "<g transform='rotate(" + this.boatCourse + " " + this.iconCenter +
+                              " " + this.iconCenter + ")'>" +
+        "<path d='" + this.path + "' fill='none' stroke-opacity='1' stroke-width='2' stroke='#ff00ff'/></g></svg>";
       const iconUrl = 'data:image/svg+xml;base64,' + btoa(svg);
       return L.icon({
         iconUrl: iconUrl,
-        iconAnchor: [11, 11],
+        iconAnchor: [this.iconCenter, this.iconCenter],
       });
     },
   },
