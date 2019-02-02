7e26df2c996063c0d60222563eaa5c996e7e1470
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2019-02-03 00:00:11 +0200

    Sail added to own boat, with a crude angle calculation

--- src/components/map/playerboat.vue
+++ src/components/map/playerboat.vue
@@ -27,8 +27,11 @@ export default {
     boatCourse () {
       return radToDeg(this.$store.state.boat.instruments.course.value);
     },
+    boatTwa () {
+      return radToDeg(this.$store.state.boat.instruments.twa.value);
+    },
     iconCenter () {
-      return 18;
+      return 20;
     },
     iconSize () {
       return this.iconCenter * 2 + 1;
@@ -39,6 +42,18 @@ export default {
              ' C ' + this.pathPos(6, 9) + ',' + this.pathPos(5, -1) + ',' + this.pathPos(3, 11) +
              ' Z';
     },
+    sailAngle () {
+      /* FIXME:
+       * perhaps some AWA based calculation could result in a better angle
+       * maxvmg angle should be consider especially for headwind
+       */
+      return this.boatTwa / (180 / 75);
+    },
+    sailPath () {
+      const curvePar = Math.sign(this.sailAngle) * -3;
+      return 'M 0 0 ' +
+             'C ' + curvePar + ' 5, ' +  curvePar + ' 12, 0 17';
+    },
     myBoatIcon() {
       const svg = "<svg xmlns='http://www.w3.org/2000/svg' width='" +
                   this.iconSize + "px' height='" +
@@ -46,7 +61,10 @@ export default {
         "<g transform='translate(" + this.iconCenter + "," + this.iconCenter +
                       ") rotate(" + this.boatCourse +
                       ")'  fill='none' stroke-opacity='1' stroke-width='2' stroke='#ff00ff'>" +
-        "<path d='" + this.boatPath + "'/></g></svg>";
+        "<path d='" + this.boatPath + "'/>" +
+          "<g transform='translate(0, -6) rotate(" + this.sailAngle +
+          ")'><path d='" + this.sailPath + "'/>" +
+        "</g></g></svg>";
       const iconUrl = 'data:image/svg+xml;base64,' + btoa(svg);
       return L.icon({
         iconUrl: iconUrl,
