1373d52552920f164c3075e2f9ed2e6197d41f15
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2019-02-02 21:37:28 +0200

    Make center unambiguous by adding +1 to size

--- src/components/map/playerboat.vue
+++ src/components/map/playerboat.vue
@@ -34,7 +34,7 @@ export default {
       return 11;
     },
     iconSize () {
-      return this.iconCenter * 2;
+      return this.iconCenter * 2 + 1;
     },
     myBoatIcon() {
       const svg = "<svg xmlns='http://www.w3.org/2000/svg' width='" +
