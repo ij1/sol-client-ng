ddfdffd828993cdc13abd4f5e93e0cfcc606e2ca
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2019-02-18 21:29:14 +0200

    Add dst & GC bearing to hover

--- src/components/map/windinfo.vue
+++ src/components/map/windinfo.vue
@@ -10,6 +10,9 @@
     <div>
       {{ this.wind }}
     </div>
+    <div>
+      {{ this.bearing }}
+    </div>
   </l-control>
 </template>
 
@@ -17,6 +20,7 @@
 import { mapState } from 'vuex';
 import { LControl } from 'vue2-leaflet';
 import { radToDeg } from '../../lib/utils.js';
+import { gcCalc } from '../../lib/nav.js';
 
 export default {
   name: 'WindInfo',
@@ -47,8 +51,18 @@ export default {
       }
       return wind.knots.toFixed(2) + 'kn @' + radToDeg(wind.twd).toFixed(2) + '\xb0';
     },
+    bearing () {
+      if (this.hoverLatLng === null) {
+        return '';
+      }
+      const gcPath = gcCalc(this.boatPosition, this.hoverLatLng);
+
+      return (radToDeg(gcPath.distance) * 60).toFixed(3) + 'nm @' +
+             radToDeg(gcPath.startBearing).toFixed(2) + '\xb0 GC';
+    },
     ...mapState({
       wxLoaded: state => state.weather.loaded,
+      boatPosition: state => state.boat.position,
     }),
   },
 }
