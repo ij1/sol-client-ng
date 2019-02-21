5faa6bd9026a939db69882db659e5250677bf210
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2019-02-21 11:49:08 +0200

    Add view update stamp to work-around need to watch many variables

--- src/store/modules/map.js
+++ src/store/modules/map.js
@@ -8,6 +8,7 @@ export default {
     zoom: 3,
     hoverLatLng: null,
     size: L.point(1, 1),
+    viewUpdateStamp: 0,
   },
 
   mutations: {
@@ -15,10 +16,12 @@ export default {
       state.center = view.center;
       state.zoom = view.zoom;
       state.bounds = view.bounds;
+      state.viewUpdateStamp++;
     },
     setSize(state, size) {
       state.size = size.size;
       state.bounds = size.bounds;
+      state.viewUpdateStamp++;
     },
     setHover(state, latLng) {
       state.hoverLatLng = latLng;
