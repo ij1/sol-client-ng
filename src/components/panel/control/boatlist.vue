ba553a7352e94b247f4b7613a5114b441edc22f1
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2019-02-22 23:54:38 +0200

    Split boatlist out from leaderboard

---
+++ src/components/panel/control/boatlist.vue
@@ -0,0 +1,212 @@
+<template>
+  <div id="boatlist-table">
+    <table cellspacing="0" cellpadding="1px">
+      <thead>
+        <th
+          v-for = "column in visibleColumnsWithSort"
+          :key = "column.dataField"
+          @click="selectSort(column.dataField, column.localeSort)"
+        >
+          {{column.thWithSort}}
+        </th>
+      </thead>
+      <tbody id="boatlist-body">
+        <tr
+          v-for = "boat in sortedBoatList"
+          :key = "boat.id"
+          :class = "{'active': selected.includes(boat.id), 'maphover': maphover.includes(boat.id)}"
+          @click.exact = "selectBoat(boat.id, false)"
+          @click.alt.exact = "selectBoat(boat.id, true)"
+        >
+          <td
+            v-for = "column in visibleColumnsWithSort"
+            :key = "column.dataField"
+            :class = "{ 'boatlist-left': column.align === 'l', 'boatlist-right': column.align === 'r' }"
+          >
+            <country-flag
+              v-if = "column.dataField === 'country'"
+              :country = "boat[column.dataField]"
+            />
+            <syc-flag
+              v-else-if = "column.dataField === 'syc'"
+              :syc = "boat[column.dataField]"
+            />
+            <span v-else>
+              {{boat[column.dataField] | prettyPrint(column) }}
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import { EventBus } from '../../../lib/event-bus.js';
+import CountryFlag from '../../countryflag.vue';
+import SycFlag from '../../sycflag.vue';
+
+export default {
+  name: 'BoatList',
+  components: {
+    'country-flag': CountryFlag,
+    'syc-flag': SycFlag,
+  },
+  props: {
+    filter: {
+      type: String,
+      default: '',
+    },
+    boatList: {
+      type: Array,
+      required: true,
+    },
+  },
+  data () {
+    return {
+      sortKey: 'ranking',
+      sortDir: 'asc',
+      localeSort: false,
+    }
+  },
+  filters: {
+    prettyPrint (value, column) {
+      if (column.dataField === 'dtg') {
+        return value.toFixed(2);
+      } else if (column.dataField === 'syc') {
+        return value === true ? 't' : 'f';
+      }
+      return value;
+    }
+  },
+  computed: {
+    columns () {
+      return [
+        {
+          dataField: 'ranking', th: '#',
+          align: 'r', visible: true, localeSort: false,
+        },
+        {
+          dataField: 'country', th: '',
+          align: 'l', visible: true, localeSort: false,
+        },
+        {
+          dataField: 'syc', th: '',
+          align: 'l', visible: true, localeSort: false,
+        },
+        {
+          dataField: 'name', th: 'Name',
+          align: 'l', visible: true, localeSort: true,
+        },
+        {
+          dataField: 'dtg', th: 'DTF',
+          align: 'r', visible: true, localeSort: false,
+        },
+        {
+          dataField: 'type', th: 'Boat Type',
+          align: 'l', visible: this.multiClassRace, localSort: false,
+        },
+      ];
+    },
+    visibleColumnsWithSort () {
+      let cols = [];
+      for (let i of this.columns) {
+        if (!i.visible) {
+          continue;
+        }
+        let sortVisualizer = '';
+        if (i.dataField === this.sortKey) {
+          sortVisualizer = this.sortDir === 'asc' ? '\u25b2' : '\u25bc';
+        }
+        cols.push({
+          ...i,
+          thWithSort: i.th + ' ' + sortVisualizer,
+        });
+      }
+      return cols;
+    },
+    sortedBoatList () {
+      const dir = this.sortDir === 'asc' ? 1 : -1;
+      const needle = this.filter.toLowerCase();
+
+      return this.boatList.filter(boat => {
+        return (this.filter.length === 0) ||
+               boat.name.toLowerCase().includes(needle);
+      }).sort((a, b) => {
+        if (!this.localeSort) {
+          if (a[this.sortKey] < b[this.sortKey]) {
+            return -dir;
+          }
+          if (a[this.sortKey] > b[this.sortKey]) {
+            return dir;
+          }
+          return 0;
+        } else {
+          return dir * a[this.sortKey].localeCompare(b[this.sortKey]);
+        }
+      });
+    },
+    selected () {
+      return this.$store.state.race.fleet.selected;
+    },
+    maphover () {
+      return this.$store.state.race.fleet.hover;
+    },
+    ...mapGetters({
+      fleetBoatFromId: 'race/fleet/boatFromId',
+      multiClassRace: 'race/fleet/multiClassRace',
+    }),
+  },
+  methods: {
+    selectSort (column, localeSort) {
+      if (this.sortKey === column) {
+        this.sortDir = (this.sortDir === 'asc' ? 'desc' : 'asc');
+      } else {
+        this.sortKey = column;
+        this.sortDir = 'asc';
+      }
+      this.localeSort = localeSort;
+    },
+    selectBoat (id, keepMapPosition) {
+      if (this.selected.includes(id)) {
+        this.$store.commit('race/fleet/setSelected', []);
+      } else {
+        this.$store.commit('race/fleet/setSelected', [id]);
+
+        const position = this.fleetBoatFromId(this.selected).latLng;
+        EventBus.$emit('map-highlight', {
+          latLng: position,
+          keepMapPosition: keepMapPosition,
+        });
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+#boatlist-table {
+  width: 100%;
+  height: calc(100% - 48px);
+  font-size: 10px;
+  white-space: nowrap;
+  overflow-y: scroll;
+}
+#boatlist-body tr {
+  background: #ffffff;
+}
+#boatlist-body .active {
+  background: #d0d0ff;
+}
+#boatlist-body .maphover {
+  background: #e0e0ff;
+}
+
+.boatlist-left {
+  text-align: left;
+}
+.boatlist-right {
+  text-align: right;
+}
+</style>
--- src/components/panel/control/leaderboard.vue
+++ src/components/panel/control/leaderboard.vue
@@ -11,180 +11,27 @@
         >
       </div>
     </div>
-    <div id="leaderboard-table">
-    <table cellspacing="0" cellpadding="1px">
-      <thead>
-        <th
-          v-for = "column in visibleColumnsWithSort"
-          :key = "column.dataField"
-          @click="selectSort(column.dataField, column.localeSort)"
-        >
-          {{column.thWithSort}}
-        </th>
-      </thead>
-      <tbody id="leaderboard-body">
-        <tr
-          v-for = "boat in boatList"
-          :key = "boat.id"
-          :class = "{'active': selected.includes(boat.id), 'maphover': maphover.includes(boat.id)}"
-          @click.exact = "selectBoat(boat.id, false)"
-          @click.alt.exact = "selectBoat(boat.id, true)"
-        >
-          <td
-            v-for = "column in visibleColumnsWithSort"
-            :key = "column.dataField"
-            :class = "{ 'leaderboard-left': column.align === 'l', 'leaderboard-right': column.align === 'r' }"
-          >
-            <country-flag
-              v-if = "column.dataField === 'country'"
-              :country = "boat[column.dataField]"
-            />
-            <syc-flag
-              v-else-if = "column.dataField === 'syc'"
-              :syc = "boat[column.dataField]"
-            />
-            <span v-else>
-              {{boat[column.dataField] | prettyPrint(column) }}
-            </span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    </div>
+    <boat-list :filter="this.filter" :boat-list="this.boatList"/>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import { EventBus } from '../../../lib/event-bus.js';
-import CountryFlag from '../../countryflag.vue';
-import SycFlag from '../../sycflag.vue';
+import BoatList from './boatlist.vue';
 
 export default {
   name: 'Leadeboard',
   components: {
-    'country-flag': CountryFlag,
-    'syc-flag': SycFlag,
+    'boat-list': BoatList,
   },
   data () {
     return {
       listname: 'Main Fleet',
       filter: '',
-      sortKey: 'ranking',
-      sortDir: 'asc',
-      localeSort: false,
-    }
-  },
-  filters: {
-    prettyPrint (value, column) {
-      if (column.dataField === 'dtg') {
-        return value.toFixed(2);
-      } else if (column.dataField === 'syc') {
-        return value === true ? 't' : 'f';
-      }
-      return value;
     }
   },
   computed: {
-    columns () {
-      return [
-        {
-          dataField: 'ranking', th: '#',
-          align: 'r', visible: true, localeSort: false,
-        },
-        {
-          dataField: 'country', th: '',
-          align: 'l', visible: true, localeSort: false,
-        },
-        {
-          dataField: 'syc', th: '',
-          align: 'l', visible: true, localeSort: false,
-        },
-        {
-          dataField: 'name', th: 'Name',
-          align: 'l', visible: true, localeSort: true,
-        },
-        {
-          dataField: 'dtg', th: 'DTF',
-          align: 'r', visible: true, localeSort: false,
-        },
-        {
-          dataField: 'type', th: 'Boat Type',
-          align: 'l', visible: this.multiClassRace, localSort: false,
-        },
-      ];
-    },
-    visibleColumnsWithSort () {
-      let cols = [];
-      for (let i of this.columns) {
-        if (!i.visible) {
-          continue;
-        }
-        let sortVisualizer = '';
-        if (i.dataField === this.sortKey) {
-          sortVisualizer = this.sortDir === 'asc' ? '\u25b2' : '\u25bc';
-        }
-        cols.push({
-          ...i,
-          thWithSort: i.th + ' ' + sortVisualizer,
-        });
-      }
-      return cols;
-    },
     boatList () {
-      const dir = this.sortDir === 'asc' ? 1 : -1;
-      const needle = this.filter.toLowerCase();
-
-      return this.$store.state.race.fleet.boat.filter(boat => {
-        return (this.filter.length === 0) ||
-               boat.name.toLowerCase().includes(needle);
-      }).sort((a, b) => {
-        if (!this.localeSort) {
-          if (a[this.sortKey] < b[this.sortKey]) {
-            return -dir;
-          }
-          if (a[this.sortKey] > b[this.sortKey]) {
-            return dir;
-          }
-          return 0;
-        } else {
-          return dir * a[this.sortKey].localeCompare(b[this.sortKey]);
-        }
-      });
-    },
-    selected () {
-      return this.$store.state.race.fleet.selected;
-    },
-    maphover () {
-      return this.$store.state.race.fleet.hover;
-    },
-    ...mapGetters({
-      fleetBoatFromId: 'race/fleet/boatFromId',
-      multiClassRace: 'race/fleet/multiClassRace',
-    }),
-  },
-  methods: {
-    selectSort (column, localeSort) {
-      if (this.sortKey === column) {
-        this.sortDir = (this.sortDir === 'asc' ? 'desc' : 'asc');
-      } else {
-        this.sortKey = column;
-        this.sortDir = 'asc';
-      }
-      this.localeSort = localeSort;
-    },
-    selectBoat (id, keepMapPosition) {
-      if (this.selected.includes(id)) {
-        this.$store.commit('race/fleet/setSelected', []);
-      } else {
-        this.$store.commit('race/fleet/setSelected', [id]);
-
-        const position = this.fleetBoatFromId(this.selected).latLng;
-        EventBus.$emit('map-highlight', {
-          latLng: position,
-          keepMapPosition: keepMapPosition,
-        });
-      }
+      return this.$store.state.race.fleet.boat;
     }
   },
 }
@@ -197,28 +44,5 @@ export default {
 }
 .leaderboard-header, .leaderboard-search, .leaderboard-search input {
   font-size: 11px;
-}
-#leaderboard-table {
-  width: 100%;
-  height: calc(100% - 48px);
-  font-size: 10px;
-  white-space: nowrap;
-  overflow-y: scroll;
-}
-#leaderboard-body tr {
-  background: #ffffff;
-}
-#leaderboard-body .active {
-  background: #d0d0ff;
-}
-#leaderboard-body .maphover {
-  background: #e0e0ff;
-}
-
-.leaderboard-left {
-  text-align: left;
-}
-.leaderboard-right {
-  text-align: right;
 }
 </style>
