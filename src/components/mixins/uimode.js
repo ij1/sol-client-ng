0ca8cdf9cd8f8c2a44d8141c6b2116d614a5bee0
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2019-07-17 22:23:42 +0300

    rename dblclicktimer to clicktimer

--- src/components/mixins/uimode.js
+++ src/components/mixins/uimode.js
@@ -6,7 +6,7 @@ export let uiModeMixin = {
   data () {
     return {
       uiModeData: {
-        dblClickTimer: null,
+        clickTimer: null,
         dblClickInterval: 200,
       },
       uiModeHandlingDblClicks: false,
@@ -21,25 +21,25 @@ export let uiModeMixin = {
     },
     uiModeOnCommitClick (e) {
       this.$emit('singleclick-committed', e);
-      this.uiModeData.dblClickTimer = null;
+      this.uiModeData.clickTimer = null;
     },
     uiModeOnClick (e) {
       if (this.uiModeHandlingDblClicks) {
-        if (this.uiModeData.dblClickTimer !== null) {
-          this.uiModeCancelDblClickTimer();
+        if (this.uiModeData.clickTimer !== null) {
+          this.uiModeCancelClickTimer();
           this.$emit('doubleclick', e);
         } else {
           this.$emit('singleclick-early', e);
-          this.uiModeData.dblClickTimer = setTimeout(this.uiModeOnCommitClick,
+          this.uiModeData.clickTimer = setTimeout(this.uiModeOnCommitClick,
                                                      this.uiModeData.dblClickInterval, e);
         }
       } else {
         this.$emit('singleclick-committed', e);
       }
     },
-    uiModeCancelDblClickTimer () {
-      clearTimeout(this.uiModeData.dblClickTimer);
-      this.uiModeData.dblClickTimer = null;
+    uiModeCancelClickTimer () {
+      clearTimeout(this.uiModeData.clickTimer);
+      this.uiModeData.clickTimer = null;
     },
   },
   mounted () {
@@ -49,8 +49,8 @@ export let uiModeMixin = {
   beforeDestroy () {
     window.removeEventListener('keyup', this.uiModeOnKey);
     this.map.off('mousedown', this.uiModeOnClick, this);
-    if (this.uiModeData.dblClickTimer !== null) {
-      this.uiModeCancelDblClickTimer();
+    if (this.uiModeData.clickTimer !== null) {
+      this.uiModeCancelClickTimer();
     }
   },
 }
