7b0e5d7f8f521fc21e377a48f31555ceb9a1ff01
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2019-08-10 12:59:40 +0300

    Re-start fate-share the poi popups work
    Mostly cherry-pick the old approach but now the open state is
    stored into vuex. Now implements also open on create.

--- src/components/map/tools/poi.vue
+++ src/components/map/tools/poi.vue
@@ -7,11 +7,16 @@
       <l-marker
         :lat-lng = "latLngAddOffset(poi.latLng, offset)"
         :options = "poiOptions"
+        ref = "popupctrl"
       >
         <l-icon
           :icon-url = "iconUrl">
         </l-icon>
-        <l-popup :options="popupOptions">
+        <l-popup
+          :options="popupOptions"
+          @add = "onOpen"
+          @remove = "onClose"
+        >
           <poi-info :poi = "poi"/>
           <form @submit.prevent = "onButterfly">
             <button type="submit">{{butterflyButtonText}}</button>
@@ -65,9 +70,11 @@ export default {
         maxWidth: 370,
         autoClose: false,
         closeOnClick: false,
+        autoPan: false,
       },
       wrapList: [-360, 0, 360],
       iconUrl: iconUrl,
+      recursionTrap: false,
     }
   },
   computed: {
@@ -82,6 +89,9 @@ export default {
     butterflyButtonText () {
       return this.poi.showButterfly ? "Hide butterfly" : "Show butterfly";
     },
+    open () {
+      return this.poi.open;
+    },
     ...mapState({
       wxTime: state => state.weather.time,
     }),
@@ -93,7 +103,43 @@ export default {
     onButterfly () {
       this.$store.commit('ui/poi/toggleButterfly', this.poi.id);
     },
+    updatePopupState () {
+      if (this.recursionTrap) {
+        return;
+      }
+      this.recursionTrap = true;
+      for (const el of this.$refs.popupctrl) {
+        if (this.open) {
+          el.mapObject.openPopup();
+        } else {
+          el.mapObject.closePopup();
+        }
+      }
+      this.recursionTrap = false;
+    },
+    onOpen () {
+      this.$store.commit('ui/poi/setOpen', {
+        id: this.poi.id,
+        open: true,
+      });
+    },
+    onClose () {
+      this.$store.commit('ui/poi/setOpen', {
+        id: this.poi.id,
+        open: false,
+      });
+    },
     latLngAddOffset,
+  },
+  watch: {
+    open () {
+      this.updatePopupState();
+    },
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.updatePopupState();
+    });
   },
 }
 </script>
--- src/store/modules/poi.js
+++ src/store/modules/poi.js
@@ -10,12 +10,21 @@ export default {
     newPoi (state, poi) {
       poi.id = state.poiId;
       poi.showButterfly = false;
+      poi.open = true;
       state.poiId++;
 
       state.pois.push(poi);
     },
     delPoi (state, id) {
       state.pois = state.pois.filter(i => i.id !== id);
+    },
+    setOpen (state, data) {
+      for (let poi of state.pois) {
+        if (poi.id === data.id) {
+          poi.open = data.open;
+          return;
+        }
+      }
     },
     toggleButterfly (state, id) {
       for (let poi of state.pois) {
