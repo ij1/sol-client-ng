e36bf417a297a53f6ade2f1e760a263b24269807
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2019-09-24 22:35:12 +0300

    Move moust of coordinate code into a mixin

--- src/components/coordinate.vue
+++ src/components/coordinate.vue
@@ -5,66 +5,14 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import { roundToFixed } from '../lib/quirks.js';
-
-const snHemispheres = ['S', 'N'];
-const weHemispheres = ['W', 'E'];
+import { coordinateMixin } from './mixins/coordinate.js';
 
 export default {
   name: 'MapCoordinate',
-  props: {
-    latLng: {
-      type: Object,
-    }
-  },
+  mixins: [ coordinateMixin ],
   computed: {
-    precision () {
-      return 5;
-    },
-    latFormatted () {
-      if ((typeof this.latLng === 'undefined') || (this.latLng === null)) {
-        return '';
-      }
-      return this.formatNumber(this.latLng.lat, snHemispheres);
-    },
-    lngFormatted () {
-      if ((typeof this.latLng === 'undefined') || (this.latLng === null)) {
-        return '';
-      }
-      return this.formatNumber(this.latLng.lng, weHemispheres);
-    },
     separator () {
       return this.signToHemisphere ? '' : ',';
-    },
-    ...mapGetters({
-      signToHemisphere: 'ui/coordinateSignToHemisphere',
-    }),
-  },
-  methods: {
-    formatNumber (value, hemispheres) {
-      const absValue = Math.abs(value);
-      let text;
-
-      if (this.coordinateFormat === 'degmin') {
-        let degs = Math.floor(absValue);
-        /* CHECKME: Precision approximation by subtracting -2 ok? */
-        let minutes = roundToFixed((absValue - degs) * 60, this.precision - 2);
-        if (minutes.startsWith('60')) {
-          degs += 1;
-          minutes = roundToFixed(0, this.precision - 2);
-        }
-        text = degs + '\xb0' + minutes + "'";
-      } else {
-        text = roundToFixed(absValue, this.precision) + '\xb0';
-      }
-
-      if (this.signToHemisphere) {
-        text = text + hemispheres[value < 0 ? 0 : 1];
-      } else if (value < 0) {
-        text = '-' + text;
-      }
-      return text;
     },
   },
 }
---
+++ src/components/mixins/coordinate.js
@@ -0,0 +1,59 @@
+import { mapGetters } from 'vuex';
+import { roundToFixed } from '../../lib/quirks.js';
+
+const snHemispheres = ['S', 'N'];
+const weHemispheres = ['W', 'E'];
+
+export let coordinateMixin = {
+  props: {
+    latLng: {
+      type: Object,
+    }
+  },
+  computed: {
+    precision () {
+      return 5;
+    },
+    latFormatted () {
+      if ((typeof this.latLng === 'undefined') || (this.latLng === null)) {
+        return '';
+      }
+      return this.formatNumber(this.latLng.lat, snHemispheres);
+    },
+    lngFormatted () {
+      if ((typeof this.latLng === 'undefined') || (this.latLng === null)) {
+        return '';
+      }
+      return this.formatNumber(this.latLng.lng, weHemispheres);
+    },
+    ...mapGetters({
+      signToHemisphere: 'ui/coordinateSignToHemisphere',
+    }),
+  },
+  methods: {
+    formatNumber (value, hemispheres) {
+      const absValue = Math.abs(value);
+      let text;
+
+      if (this.coordinateFormat === 'degmin') {
+        let degs = Math.floor(absValue);
+        /* CHECKME: Precision approximation by subtracting -2 ok? */
+        let minutes = roundToFixed((absValue - degs) * 60, this.precision - 2);
+        if (minutes.startsWith('60')) {
+          degs += 1;
+          minutes = roundToFixed(0, this.precision - 2);
+        }
+        text = degs + '\xb0' + minutes + "'";
+      } else {
+        text = roundToFixed(absValue, this.precision) + '\xb0';
+      }
+
+      if (this.signToHemisphere) {
+        text = text + hemispheres[value < 0 ? 0 : 1];
+      } else if (value < 0) {
+        text = '-' + text;
+      }
+      return text;
+    },
+  },
+}
