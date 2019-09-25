2b1c07e574a98f6db05bd642e5e98c21716e6b0a
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2019-09-25 20:59:46 +0300

    Move distance filter to mixin

--- src/components/distance.vue
+++ src/components/distance.vue
@@ -11,28 +11,17 @@
 <script>
 import { radToDeg } from '../lib/utils.js';
 import { roundToFixed } from '../lib/quirks.js';
-import { EARTH_R } from '../lib/sol.js';
+import { distanceMixin } from './mixins/distance.js';
 
 export default {
   name: 'PathDistance',
+  mixins: [distanceMixin],
   props: {
     path: {
       type: Object,
     }
   },
   filters: {
-    distance (value) {
-      let decimals = 3;
-      let nm = value * EARTH_R / 1852;
-      if (nm >= 1000) {
-        decimals = 0;
-      } else if (nm >= 100) {
-        decimals = 1;
-      } else if (nm >= 10) {
-        decimals = 2;
-      }
-      return roundToFixed(nm, decimals);
-    },
     bearing (value) {
       return roundToFixed(radToDeg(value), 2);
     },
---
+++ src/components/mixins/distance.js
@@ -0,0 +1,19 @@
+import { roundToFixed } from '../../lib/quirks.js';
+import { EARTH_R } from '../../lib/sol.js';
+
+export let distanceMixin = {
+  filters: {
+    distance (value) {
+      let decimals = 3;
+      let nm = value * EARTH_R / 1852;
+      if (nm >= 1000) {
+        decimals = 0;
+      } else if (nm >= 100) {
+        decimals = 1;
+      } else if (nm >= 10) {
+        decimals = 2;
+      }
+      return roundToFixed(nm, decimals);
+    },
+  },
+}
