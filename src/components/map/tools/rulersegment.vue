af2f17efbc8da336e08e8306b4e3cd5ed1c81d1d
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2019-09-30 11:06:31 +0300

    GC ruler

--- src/components/map/tools/rulersegment.vue
+++ src/components/map/tools/rulersegment.vue
@@ -46,7 +46,8 @@
 <script>
 import { mapState, mapGetters } from 'vuex';
 import { LLayerGroup, LPolyline, LTooltip, LCircleMarker } from 'vue2-leaflet';
-import { radToDeg, latLngArrayAddOffset } from '../../../lib/utils.js';
+import L from 'leaflet';
+import { degToRad, radToDeg, latLngArrayAddOffset } from '../../../lib/utils.js';
 import { distanceMixin } from '../../mixins/distance.js';
 import PathDistance from '../../distance.vue';
 
@@ -83,6 +84,12 @@ export default {
     },
     lines () {
       let res = [];
+      let line = this.segment.line;
+
+      if (this.segment.navMode === 'ortho') {
+        line = this.gcLine();
+      }
+
       let wrapList = this.worldCopyWrap ? this.mapWrapList : [0];
       // ADDME: when the second point crosses anti-meridian compared with
       // the first point, we need to add more copies
@@ -90,7 +97,7 @@ export default {
       for (const offset of wrapList) {
         res.push({
           offset: offset,
-          line: latLngArrayAddOffset(this.segment.line, offset),
+          line: latLngArrayAddOffset(line, offset),
         });
       }
       return res;
@@ -145,6 +152,49 @@ export default {
     ...mapGetters({
       rulerExtendingPath: 'ui/ruler/extendingPath',
     }),
+  },
+  methods: {
+    gcLine () {
+      if (this.zeroLen) {
+        return this.segment.line;
+      }
+      const bearing1 = this.segment.startBearing;
+      const lat1 = degToRad(this.segment.line[0].lat);
+      const lon1 = degToRad(this.segment.line[0].lng);
+      const sina1 = Math.sin(bearing1);
+      const cosa1 = Math.cos(bearing1);
+      const slat1 = Math.sin(lat1);
+      const clat1 = Math.cos(lat1);
+      const dividend = sina1 * clat1;
+      const divisor = cosa1 * cosa1 + sina1 * sina1 * slat1 * slat1;
+      const bearing0 = Math.atan2(dividend, Math.sqrt(divisor));
+
+      let dist01;
+      if ((lat1 === 0) && (bearing1 === Math.PI / 2)) {
+        dist01 = 0;
+      } else {
+        dist01 = Math.atan2(Math.tan(lat1), cosa1);
+      }
+      const lon0 = lon1 - Math.atan2(Math.sin(bearing0) * Math.sin(dist01), Math.cos(dist01));
+
+      let res = [this.segment.line[0]];
+      if (bearing0 !== 0) {
+        const cot0 = 1 / Math.tan(bearing0);
+        const lon2 = degToRad(this.segment.line[this.segment.line.length - 1].lng);
+        const dlon = lon2 - lon1;
+        const iterCount = 150;
+        const lonStep = dlon / iterCount;
+        let lon = lon1;
+        for (let i = 0; i < iterCount; i++) {
+          lon += lonStep;
+          let lat = Math.atan(cot0 * Math.sin(lon - lon0));
+          res.push(L.latLng(radToDeg(lat), radToDeg(lon)));
+        }
+      }
+      res.push(this.segment.line[this.segment.line.length - 1]);
+
+      return res;
+    },
   },
 }
 </script>
