88f1c5f8f3bd8aebff9661b35f6b9f46c59b2791
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-01-18 00:09:26 +0200

    Only hook the up handler when necessary

--- src/components/map/buttons/holdrepeat.js
+++ src/components/map/buttons/holdrepeat.js
@@ -13,19 +13,19 @@ export let holdRepeatMixin = {
     holdRepeatStart (e) {
       this.holdRepeatEv = e;
       this.map.on('zoomend', this.holdRepeatOnRepeat);
+      window.addEventListener('mouseup', this.holdRepeatStop);
     },
     holdRepeatStop () {
       this.holdRepeatEv = null;
       this.map.off('zoomend', this.holdRepeatOnRepeat);
+      window.removeEventListener('mouseup', this.holdRepeatStop);
     },
   },
   mounted () {
-    window.addEventListener('mouseup', this.holdRepeatStop);
   },
   beforeDestroy () {
-    window.removeEventListener('mouseup', this.holdRepeatStop);
     if (this.holdRepeatEv !== null) {
-      this.map.off('zoomend', this.holdRepeatOnRepeat);
+      this.holdRepeatStop();
     }
   },
 }
