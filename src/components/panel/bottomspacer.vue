3bf4cc573573912f5f9122b97997ca48390d1039
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-01-25 23:38:15 +0200

    Try to solve iPad desktop faking
    The swipebar is obscuring instruments on iPad and Safari asks
    for desktop site, hiding ipad from the user agent.

--- src/components/panel/bottomspacer.vue
+++ src/components/panel/bottomspacer.vue
@@ -3,13 +3,16 @@
 </template>
 
 <script>
+import L from 'leaflet';
+
 export default {
   name: 'BottomSpacer',
   computed: {
     bottomSpacerSize () {
       const userAgent = window.navigator.userAgent.toLowerCase();
 
-      if (/ipad/.test(userAgent)) {
+      if ((/ipad/.test(userAgent)) ||
+          (/safari/.test(userAgent) && L.Browser.mobile)) {
         return '12px';
       }
       return '0';
