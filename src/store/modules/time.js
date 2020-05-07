83e463497cfcc79e889435befe21f446db77e0e3
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-05-07 13:05:42 +0300

    Simple time offset approximation
    Not perfect but then 1 sec precision might not really be necessary
    for the task here.

--- src/store/modules/time.js
+++ src/store/modules/time.js
@@ -1,3 +1,5 @@
+import { UTCToMsec, secToMsec } from '../../lib/utils.js';
+
 function secTimer (state, commit) {
   commit('update');
   const delay = 1000 - ((state.realTime + 2) % 1000);
@@ -18,6 +20,9 @@ export default {
       state.realTime = Date.now() + state.clockOffset;
       state.siteTime = performance.now();
     },
+    applyCorrection (state, correction) {
+      state.clockOffset = correction;
+    }
   },
 
   getters: {
@@ -30,8 +35,40 @@ export default {
   },
 
   actions: {
-    init ({state, commit}) {
+    async init ({state, dispatch, commit}) {
       secTimer(state, commit);
+      await dispatch('checkOffset');
+    },
+    async checkOffset ({dispatch, commit}) {
+      const getDef = {
+        apiCall: 'time',
+        url: '/webclient/time.xml',
+        useArrays: false,
+        dataField: 'response',
+      };
+      try {
+        const preNow = Date.now();
+        const data = await dispatch('solapi/get', getDef, {root: true});
+        const postNow = Date.now();
+        const peerNow = UTCToMsec(data.time);
+
+        const lowCorrection = (preNow - secToMsec(1)) - peerNow;
+        const highCorrection = peerNow - (postNow + secToMsec(1));
+        const correction = (lowCorrection > 0) ? -lowCorrection :
+                           (highCorrection > 0) ? highCorrection : 0;
+        if (correction !== 0) {
+          commit('applyCorrection', correction);
+          dispatch('diagnostics/add', 'Clock correction: ' + correction,
+                   {root: true});
+        } else {
+          dispatch('diagnostics/add', 'Time check OK!', {root: true});
+        }
+      } catch (err) {
+        commit('solapi/logError', {
+          request: getDef,
+          error: err,
+        }, {root: true});
+      }
     },
   },
 }
