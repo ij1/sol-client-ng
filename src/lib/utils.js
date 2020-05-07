6ed6cef74265674b32fa7864e1d7486fc6238640
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-05-07 19:11:06 +0300

    Parse also usecs taking msecs part when available

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,20 +2,26 @@ import L from 'leaflet';
 import { store } from '../store/index.js';
 
 export function UTCToMsec(dateString) {
-  const regex = /^(\d{4})\/([01]\d)\/([0-3]\d) ([012]\d):([0-5]\d):([0-5]\d)/;
+  const regex = /^(\d{4})\/([01]\d)\/([0-3]\d) ([012]\d):([0-5]\d):([0-5]\d)(\.(\d{3})\d{3})?/;
 
   const s = regex.exec(dateString);
   if (s === null) {
     return null;
   }
-  const ms = Date.UTC(s[1], parseInt(s[2])-1, s[3], s[4], s[5], s[6]);
+  if (typeof s[7] === 'undefined') {
+    s[8] = 0;
+  }
+  s[7] = 0;
+  const ms = Date.UTC(s[1], parseInt(s[2])-1, s[3], s[4], s[5], s[6], s[8]);
 
   if (isNaN(ms)) {
     return null;
   }
   const d = new Date(ms);
   const check = [d.getUTCFullYear(), d.getUTCMonth()+1, d.getUTCDate(),
-                 d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds()];
+                 d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds(),
+                 s[7],
+                 d.getUTCMilliseconds()];
   for (let i = 0; i < check.length; i++) {
     if (check[i] !== parseInt(s[i+1])) {
       return null;
