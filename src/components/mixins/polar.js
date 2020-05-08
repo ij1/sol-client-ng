1c91398bd4918ad6f8d14aa7d91014043154361c
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-05-08 14:07:56 +0300

    Allow speed based cut-off in drawing

--- src/components/mixins/polar.js
+++ src/components/mixins/polar.js
@@ -6,13 +6,18 @@ export let polarMixin = {
         y: -Math.cos(twa) * (speed * gridScale + extraPixels),
       };
     },
-    drawPolarCurve(ctx, curve, gridScale) {
+    drawPolarCurve(ctx, curve, gridScale, cutOff = Number.MAX_VALUE) {
       ctx.beginPath();
-      let first = true;
+      let move = true;
       for (let pt of curve.values) {
+        if (pt.speed > cutOff) {
+          move = true;
+        }
         const polarPos = this.polarCoords(pt.twa, pt.speed, gridScale);
-        first ? ctx.moveTo(polarPos.x, polarPos.y) : ctx.lineTo(polarPos.x, polarPos.y);
-        first = false;
+        move ? ctx.moveTo(polarPos.x, polarPos.y) : ctx.lineTo(polarPos.x, polarPos.y);
+        if (pt.speed < cutOff) {
+          move = false;
+        }
       }
       ctx.stroke();
     },
