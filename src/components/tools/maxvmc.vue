62510f4b9e63ca4ea10382a191a09fe921fced27
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2021-05-30 22:32:56 +0300

    Update tack automatically from instruments

--- src/components/tools/maxvmc.vue
+++ src/components/tools/maxvmc.vue
@@ -187,6 +187,11 @@ export default {
         this.updateFromInstruments();
       }
     },
+    boatTwa () {
+      if (this.fromInstruments) {
+        this.updateTack();
+      }
+    },
     currentTarget () {
       if (this.fromInstruments) {
         this.updateFromInstruments();
